d7b286b7e42305a52a80ca743dbb2d1bae5dfa9f
Administrator <Administrator@PC241>
2020-06-01 11:27:44 +0700

    Add project files.

---
+++ HCH_LAB456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HCH_LAB456.MvcApplication" Language="C#" %>
---
+++ HCH_LAB456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HCH_LAB456.Startup))]
+namespace HCH_LAB456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
